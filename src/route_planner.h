d0189e27e9479d04c5de2e4d08fad9640f5f1445
Aslanfmh65 <aslanfmh@gmail.com>
2019-05-29 01:45:13 +0000

    first commit

---
+++ src/route_planner.h
@@ -0,0 +1,33 @@
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "route_model.h"
+
+
+class RoutePlanner {
+  public:
+    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    // Add public variables or methods declarations here.
+    float GetDistance() const{return distance; }
+    void AStarSearch();
+    
+
+  private:
+    // Add private variables or methods declarations here.
+    void AddNeighbors(RouteModel::Node *current_node);
+    RouteModel &m_Model;
+    RouteModel::Node *start_node;
+    RouteModel::Node *end_node;
+    float distance;
+    std::vector<RouteModel::Node*> open_list;
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+    RouteModel::Node *NextNode();
+    float CalculateHValue(RouteModel::Node *node);
+    
+};
+
+
+#endif
